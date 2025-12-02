b79ef727d42fa1f870c4595ecf7fa72e1628e894
Johannmbo <mouleojohann@gmail.com>
2025-12-02 07:23:39 +0300

    Fix

--- models/mistral_llm_client.py
+++ models/mistral_llm_client.py
@@ -1,34 +1,25 @@
 from langchain_mistralai.chat_models import ChatMistralAI
-
 from typing import List, Dict
-# from utils.config import Config
 from dotenv import load_dotenv
 import os
-
 from models.base_llm_client import BaseLLMClient
 
 class MistralLLMClient(BaseLLMClient):
     def __init__(self):
+        load_dotenv()
+        self.API_KEY = os.getenv("MISTRAL_API_KEY")
+        if not self.API_KEY:
+            raise ValueError("MISTRAL_API_KEY not found!")
+        print("API key loaded successfully!")
 
-        self.API_KEY = None
-        load_dotenv(".env")
-        API_KEY = os.environ.get("mistral")
-
-        if not API_KEY:
-            raise ValueError("❌ MISTRAL_API_KEY not found!")
-        else:
-            print("✅ API key loaded successfully!")
-            self.API_KEY = API_KEY
-    
     async def generate(self, messages: List[Dict], model: str = "mistral-small-latest", temperature: float = 0.7) -> str:
         try:
             llm = ChatMistralAI(
-                api_key = self.API_KEY,
-                model=model,
+                mistral_api_key=self.API_KEY,
+                model_name=model,
                 temperature=temperature,
             )
-            result = llm.invoke(messages)
+            result = await llm.ainvoke(messages)
             return result.content
-
         except Exception as e:
             raise Exception(f"LLM API error: {str(e)}")
